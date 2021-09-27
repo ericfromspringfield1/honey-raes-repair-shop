9b20ad75bb23820c348e2675339c60e878a34c1e
Eric Taylor <ericfromspringfield@gmail.com>
2021-09-27 13:28:10 -0500

    added ternary conditional for styling

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,57 +1,66 @@
-import React, { useEffect, useState } from "react"
-import { useHistory } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./Tickets.css";
 
 export const TicketList = () => {
-    const [tickets, updateTickets] = useState([])
-    const [active, setActive] = useState("")
-    const history = useHistory()
-    
-    useEffect(
-        () => {
-            fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
-                .then(res => res.json())
-                .then((data) => {
-                    updateTickets(data)
-                })
-        },
-        []
+  const [tickets, updateTickets] = useState([]);
+  const [active, setActive] = useState("");
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch(
+      "http://localhost:8088/serviceTickets?_expand=employee&_expand=customer"
     )
+      .then((res) => res.json())
+      .then((data) => {
+        updateTickets(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        const activeTicketCount = tickets.filter(t => t.dateCompleted === "").length
-        setActive(`There are ${activeTicketCount} open tickets`)
-    },
-    [tickets])
+  useEffect(() => {
+    const activeTicketCount = tickets.filter(
+      (t) => t.dateCompleted === ""
+    ).length;
+    setActive(`There are ${activeTicketCount} open tickets`);
+  }, [tickets]);
 
-    
-    return (
-        <>
-                    <h1 className="customersHeading">Tickets</h1>
-                    <div>
-                    <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
-                    </div>
-                    { active }
-                    
-            {
-                tickets.map(
-                    (ticket) => {
-                    if (ticket.emergency === true){
-                        return <div key={`ticket--${ticket.id}`} style={{backgroundColor: "pink"}}>
-        
-                            <p>{ticket.description}</p>
-                            <p>submitted by {ticket.customer.name} and worked on by {ticket.employee.name}</p>
-                        </div>
-                        }else {
-                            return <div key={`ticket--${ticket.id}`}>
-        
-                            <p>{ticket.description}</p>
-                            <p>submitted by {ticket.customer.name} and worked on by {ticket.employee.name}</p>
-                        </div>
-                        }
-                        
-                    }
-                )
-            }
+  return (
+    <>
+      <h1 className="customersHeading">Tickets</h1>
+      <div>
+        <button onClick={() => history.push("/tickets/create")}>
+          Create Ticket
+        </button>
+      </div>
+      {active}
+
+      {tickets.map((ticket) => {
+          return (
+              <>
+        <div key={`ticket--${ticket.id}`}>
+        <p className={ ticket.emergency ? "emergency" : "ticket" }>   
+          {ticket.emergency ? "🚑" : ""} {ticket.description} submitted by{" "}
+          {ticket.customer.name} and worked on by {ticket.employee.name}
+        </p>
+        </div>  
         </>
-    )
-}
+       
+          )
+        // -- TERNARY CONDITIONAL ABOVE -- --STANDARD IF STATEMENT BELOW -- 
+        //  if (ticket.emergency === true){
+        //     return <div key={`ticket--${ticket.id}`} style={{backgroundColor: "pink"}}>
+
+        //         <p>{ticket.description}</p>
+        //         <p>submitted by {ticket.customer.name} and worked on by {ticket.employee.name}</p>
+        //     </div>
+        //     }else {
+        //         return <div key={`ticket--${ticket.id}`}>
+
+        //         <p>{ticket.description}</p>
+        //         <p>submitted by {ticket.customer.name} and worked on by {ticket.employee.name}</p>
+        //     </div>
+        //     }
+      })}
+    </>
+  );
+};
